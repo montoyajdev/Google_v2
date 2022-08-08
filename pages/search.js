007cfd87aca8472970ef77c768207b68efeef655
Jonathan Montoya <montoyajdev@gmail.com>
2022-08-08 18:18:00 -0500

    create pages/Search.js

---
+++ pages/search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+function Search() {
+	return (
+		<div>
+			<Head>
+				<title>Search Results</title>
+			</Head>
+
+			{/*HEADER*/}
+			{/*Search Results*/}
+		</div>
+	);
+}
+
+export default Search;
